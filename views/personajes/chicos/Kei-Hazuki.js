2beabb98fad4cbc80a8dbd72a9e666952e26b868
Dan13l4 <kdbaron426@gmail.com>
2022-04-09 19:51:35 -0500

    Creating hazuki page

--- views/personajes/chicos/Kei-Hazuki.js
+++ views/personajes/chicos/Kei-Hazuki.js
@@ -3,15 +3,195 @@ export default () => {
     <div class="todito">
             <div class="itemsTres">
                 <button class="btnes" id="volver"><img src="./img/volver.png" alt="volver" class="volve"></button>
-                <div class="conteinarTokiUno">
+                <div class="conteinarTokiUnoDos">
                 <br>
                 <br>
-                    <p>
-                      <h3 class="tituloantesde">Kazuki Kei</h3>
+                    
+                    <h2 class="tituloantesde">Hazuki Kei</h2>
                     <br>
                     <img src="./img/chicos/Hazuki-Kei.jpg" alt="Hazuki" class="Hazuki">
                     <br>
-                    
+                    <br>
+                    <p class="hazukiBio">
+                    <b class="greenTitle">Cumpleaños:</b> 16 de Octubre<br>
+                    <b class="greenTitle">Estatura:</b> 178/180/180<br>
+                    <b class="greenTitle">Peso</b> 62/63/64<br>
+                    <b class="greenTitle">Tipo de sangre:</b> A<br>
+                    <b class="greenTitle">Horoscopo:</b> Libra<br>
+                    <b class="greenTitle">Club:</b> Ninguno<br>
+                    <b class="greenTitle">Trabajo:</b> Modelo de revista<br>
+                    <b class="greenTitle">Rival:</b> ninguna<br>
+                    <b class="greenTitle">Estilos favoritos:</b> Puro, deportivo<br>
+                    <b class="greenTitle">Estilo odiado:</b> Sexy<br>
+                    <b class="greenTitle">Color favorito:</b> Blanco<br>
+                    <b class="greenTitle">Destino:</b> Universidad de primer rango<br>
+                    <b class="greenTitle">Seiyuu:</b> Midorikawa Hikaru<br>
+                    </p>
+                </div>
+                <div class="contenedorhazuki">
+                <h4 class="titulodescripcion">Descripcion:</h4>
+                <p>Hazuki es el personaje principal de este juego. Es el amigo de la infacia de la protagonista, es el esteriotipo de chico perfecto
+                en todos los ambitos. Es un chico bastante frio, pero a medida que lo vas conociendo se ira volviendo mas calido contigo.</p>
+                <br>
+                <h3 class="titulodescripcion">Datos importantes:</h3>
+                <br>
+                <b class="greenTitle">Como lo puedes llamar:</b>  <br>
+                <b>:I y por encima: </b> Hazuki, Hazuki-san, Kei-kun<br>
+                <b>:) y por encima: </b> Kei, Kei-chan<br>
+                <b>:D y por encima: </b> Kei-tama, Kei-pon<br>
+                <b>Nunca lo llames: </b> Oujisama<3/Principe<br>
+                <br>
+                <b class="greenTitle">Requisitos para conocerlo:</b>  <br>
+                Ninguno, lo conoces al principio del juego.<br>
+                <br>
+                <b class="greenTitle">Requisitos para su final:</b>  <br>
+                <ul class="citasHazuki">
+                <li>Haber ido a mas de 10 citas con el y tener un estado de amor</li>
+                <li><b>Artes:</b> 150 o mas</li>
+                <li><b>Estilo:</b> 150 o mas</li>
+                <li><b>Social:</b> 100 o mas</li>
+                <li><b>Encanto:</b> 100 o mas</li>
+                <li><b>Inteligencia:</b> 150 o mas</li>
+                <li><b>Fitness:</b> 150 o mas</li>
+                </ul>
+                <br>
+                <b class="greenTitle">Regalos favoritos por su cumpleaños:</b>  <br>
+                <b>Primer año:</b> Cat Jigsaw Puzzle/Rompecabezas de gato (1 yen)<br>
+                <b>Segundo año:</b> Cat Ear Pillow/Almohada de orejas de gato (1 yen)<br>
+                <b>Tercer año:</b> Silver Glass Accessory/ Accesorio de vidrio (3Yen)<br>
+                <br>
+                <b class="greenTitle">Regalo favorito de navidad:</b>  <br>
+                <b>10 yenes: </b>Accesorio de vidrio<br>
+                <br>
+                <h3 class="titulodescripcion">Citas:</h3>
+                <br>
+                <b class="greenTitle">Lugares favoritos:</b>  <br>
+                <ul class="citasHazuki">
+                  <li>Planetario/Planetarium</li>
+                  <li>Cinema(Velocity of Light)</li>
+                  <li>Cinema (Spinning)</li>
+                  <li>Parque forestal (Primavera)/Forest Park (Cherry Blossoms)</li>
+                  <li>Parque forestal (Verano)/Forest Park (Summer)</li>
+                  <li>Parque de las fuentes (Brick Road)/Waterfront Park (Brick Road)</li>
+                  <li>Festival de fuegos artificiales/Fireworks Festival</li>
+                  <li>Parque de atracciones (Desfile nocturno)/Amusement Park (Night Parade)</li>
+                  <li>Zoologico/Zoo</li>
+                  <li>Zoologico (Cachorros de león)/Zoo (Lion Cubs)</li>
+                </ul>
+                <br>
+                <b class="greenTitle">Lugares odiados:</b>  <br>
+                <ul class="citasHazuki">
+                  <li>Salon de eventos (Orquesta KCH)/Event Hall (KCH Orchestra)</li>
+                  <li>Parque de atracciones (Rueda de la fortuna)/Amusement Park (Merry-go-round)</li>
+                  <li>Cinema (Princesa en Peligro, Saraba Singing Princess, Girl of the Green Grass)</li>
+                  <li>Zona de bolos(tenis de mesa)/Bowling Alley (table tennis)</li>
+                  <li>Entrada al parque (tiendas)/Entrance to the park (shops)</li>
+                </ul>
+                <br>
+                <h3 class="titulodescripcion">CG'S:</h3>
+                <br>
+                <b class="greenTitle">En la escuela:</b><br>
+                <ol class="citasHazuki">
+                  <li><b>¿Necesitas una mano?</b><br>
+                  <b>Fecha:</b> Ceremonia de entrada<br>
+                  <b>Condiciones:</b> Automático<br>
+                  <b>Afecto:</b> -</li><br>
+
+                  <li><b>Por teléfono con la familia</b><br>
+                  <b>Fecha:</b> Ocurre durante el Festival de Cultura Escolar<br>
+                  <b>Condiciones:</b> Automático<br>
+                  <b>Afecto:</b> -</li><br>
+
+                  <li><b>jugando con gatos</b><br>
+                  <b>Fecha:</b> día escolar<br>
+                  <b>Condiciones:</b> Ocurre en un día que no tienes trabajo<br>
+                  <b>Afecto:</b> :D y superior<br>
+                  (Dos versiones disponibles: Verano e Invierno)</li><br>
+
+                  <li><b>En el cafe</b><br>
+                  <b>Fecha:</b> -<br>
+                  <b>Condiciones:</b> Invítalo a un café después de la escuela.<br>
+                  <b>Afecto:</b> -<br>
+                  (Dos versiones disponibles: Verano e Invierno)</li><br>
+
+                  <li><b>Obra escolar</b><br>
+                  <b>Fecha:</b> Año 3, Último día del Festival de Cultura.<br>
+                  <b>Condiciones:</b> No faltar a las prácticas del festival de cultura.<br>
+                  <b>Afecto:</b> :) y superior</li><br>
+                </ol>
+                <br>
+                <b class="greenTitle">Citas:</b><br>
+                <ol class="citasHazuki">
+                  <li><b>Aparece el rival</b><br>
+                  <b>Fecha:</b> Año 2 de mayo en adelante, Primavera<br>
+                  <b>Condiciones:</b> Cita en la bolera<br>
+                  <b>Afecto:</b> :) o superior</li><br>
+
+                  <li><b>Pensamientos sofocantes</b><br>
+                  <b>Fecha:</b>Primer fin de semana de agosto<br>
+                  <b>Condiciones:</b>Invitarlo a ver los fuegos artificiales<br>
+                  <b>Afecto:</b> :D o superior</li><br>
+
+                  <li><b>KAIWARE mogumogu</b><br>
+                  <b>Fecha:</b> Otoño<br>
+                  <b>Condiciones:</b>Ir a una cita en el parque forestal<br>
+                  <b>Afecto:</b> :| o por encima</li><br>
+
+                  <li><b>En la rueda de la fortuna</b><br>
+                  <b>Fecha:</b> 2º año en enero en adelante, Invierno<br>
+                  <b>Condiciones:</b> Invitarlo a un cita en el Seaside Park<br>
+                  <b>Afecto:</b> :D o superior</li><br>
+
+                  <li><b>Ayudando en el Flea Market</b><br>
+                  <b>Fecha:</b> agosto<br>
+                  <b>Condiciones:</b> Ir solo al mercado de pulgas, debe haber desbloqueado su tema de conversación "Acerca de los pasatiempos".<br>
+                  <b>Afecto:</b> :| o por encima</li><br>
+
+                  <li><b>Cerca de ti</b><br>
+                  <b>Fecha:</b> primavera (sin que se vean las flores)<br>
+                  <b>Condiciones:</b> Cita en el Parque Forestal<br>
+                  <b>Afecto:</b> :) o superior</li><br>
+
+                  <li><b>Árbol junto al mar a la luz de la lunat</b><br>
+                  <b>Fecha:</b> Fiesta de Navidad, Año 3<br>
+                  <b>Condiciones:</b> Automático<br>
+                  <b>Afecto:</b> :D o superior</li><br>
+
+                  <li><b>¿¡Modelando con Hazuki-kun!? (Solo DS)</b><br>
+                  <b>Fecha:</b> En un día que tengas trabajo.<br>
+                  <b>Condiciones:</b> Trabajar en ALUCARD, y tener el parámetro Charm a 120.<br>
+                  <b>Afecto:</b> :D o superior</li><br>
+                </ol>
+                <br>
+                <b class="greenTitle">Finales:</b><br>
+                <ol class="citasHazuki">
+                  <li><b>Confesión</b><br>
+                  <b>Fecha:</b> Graduación<br>
+                  <b>Condiciones:</b>Cumplir con los requisitos de confesión<br>
+                  <b>Afecto:</b> :Tokimeki</li><br>
+
+                  <li><b>Confesión 2</b><br>
+                  <b>Fecha:</b>Graduación<br>
+                  <b>Condiciones:</b>Cumplir con los requisitos de confesión (No toques sus labios para obtener CG 17)<br>
+                  <b>Afecto:</b> Tokimeki</li><br>
+
+                  <li><b>Una promesa de siempre</b><br>
+                  <b>Fecha:</b>Graduación<br>
+                  <b>Condiciones:</b>Cumplir con los requisitos de confesión<br>
+                  <b>Afecto:</b> Tokimeki</li><br>
+
+                  <li><b>Promesa en la capilla (solo DS)</b><br>
+                  <b>Fecha:</b> Graduación<br>
+                  <b>Condiciones:</b>Toca sus labios<br>
+                  <b>Afecto:</b> Tokimeki</li><br>
+
+                  <li><b>Recuerdos en la playa</b><br>
+                  <b>Fecha:</b> Graduación<br>
+                  <b>Condiciones:</b> Consigue su final "Mejor amigo"<br>
+                  <b>Afecto:</b> mejor amigo</li><br>
+                </ol>
+                <br>
+                <br>
                 </div>
             </div>
         </div>
